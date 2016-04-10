2ed8188052ea2fae50d3c1f5b1d0a60cccbf8ca7
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2016-04-10 23:12:26 +0300

    Delete some misguided attempts.

--- rs-vm/src/main.rs
+++ rs-vm/src/main.rs
@@ -1,21 +1,12 @@
-#![feature(plugin)]
-#![plugin(peg_syntax_ext)]
-
 mod gc;
 mod bytecode;
 mod vm;
-mod compiler;
-peg_file! grammar("grammar.rustpeg");
 
-// use gc::{Value, DeflatedProcedure};
-// use bytecode::{local, add, sub, mul, div, fun, call, halt, l, f, c};
-// use vm::VM;
-use grammar::expr;
-use compiler::cps;
+use gc::{Value, DeflatedProcedure};
+use bytecode::{local, add, sub, mul, div, fun, call, halt, l, f, c};
+use vm::VM;
 
 fn main() {
-    println!("{:?}", cps(expr("foo").unwrap()));
-    
     // // (let ((x (* (+ 2 3) (- 6 2)))
     // //       (y (- 2 3)))
     // //   ((fn (z) (/ z y)) x))
@@ -39,33 +30,18 @@ fn main() {
     //     clover_count: 0
     // };
 
-    // // (* (+ 2 3) (- 6 2))
-    // let simp_arith = DeflatedProcedure {
-    //     instrs: vec![add(c(0), c(1)), // 2 + 3 = 5
-    //                  sub(c(2), c(0)), // 6 - 2 = 4
-    //                  mul(l(0), l(1)), // 5*4 = 20
-    //                  halt(l(2))],     // 20
-    //     consts: vec![Value::Int(2), Value::Int(3), Value::Int(6)],
-    //     codeobjs: vec![],
-    //     clover_count: 0
-    // };
+    // (* (+ 2 3) (- 6 2))
+    let simp_arith = DeflatedProcedure {
+        instrs: vec![add(c(0), c(1)), // 2 + 3 = 5
+                     sub(c(2), c(0)), // 6 - 2 = 4
+                     mul(l(0), l(1)), // 5*4 = 20
+                     halt(l(2))],     // 20
+        consts: vec![Value::Int(2), Value::Int(3), Value::Int(6)],
+        codeobjs: vec![],
+        clover_count: 0
+    };
 
-    // let vm = VM::new();
-    // let mut vmproc = vm.spawn(&simp_arith); // 'inflates' the bytecode
-    // println!("{:?}", vmproc.run().unwrap().get_int().unwrap());
-
-    // // (* (+ 2 3) (- 6 2))
-    // let simp_arith = DeflatedProcedure {
-    //     instrs: vec![add(c(0), c(1)), // 2 + 3 = 5
-    //                  sub(c(2), c(0)), // 6 - 2 = 4
-    //                  mul(l(0), l(1)), // 5*4 = 20
-    //                  halt(l(2))],     // 20
-    //     consts: vec![Value::Int(2), Value::Int(3), Value::Int(6)],
-    //     codeobjs: vec![],
-    //     clover_count: 0
-    // };
-
-    // let vm = VM::new();
-    // let mut vmproc = vm.spawn(&simp_arith); // 'inflates' the bytecode
-    // println!("{:?}", vmproc.run().unwrap().get_int().unwrap());
+    let vm = VM::new();
+    let mut vmproc = vm.spawn(&simp_arith); // 'inflates' the bytecode
+    println!("{:?}", vmproc.run().unwrap().get_int().unwrap());
 }
