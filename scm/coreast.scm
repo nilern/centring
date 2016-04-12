0decf5bd9eee109580feec1ad29f67bdf50ea9ef
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2016-04-12 22:55:24 +0300

    Make coreast.scm support all special forms in use atm.

--- scm/coreast.scm
+++ scm/coreast.scm
@@ -2,15 +2,30 @@
   *
   
   (import scheme chicken)
-  (use (only matchable match)
+  (use (only matchable match match-lambda)
        (only (srfi 13) string-prefix? string-index))
+
+  ;;;; Utils
+
+  (define (update-nth n f ls)
+    (letrec ((udn (lambda (n vs)
+                    (if (zero? n)
+                      (cons (f (car vs)) (cdr vs))
+                      (cons (car vs) (udn (sub1 n) (cdr vs)))))))
+      (udn n ls)))
+
+  (define update-defnbody (cute update-nth 3 <> <>))
   
   ;;;; AST
 
+  (define-record Def name val)
   (define-record If cond then else)
+  (define-record Do stmts)
   (define-record Fn formals types body)
+  (define-record Fix defns body)
   (define-record Primop op args)
   (define-record App callee args)
+  (define-record Halt val)
 
   (define-record Var name)
   (define-record Const val)
@@ -32,14 +47,14 @@
 
   (define (analyze sexp)
     (match sexp
-      ((op . args)
-       (match op
-         ((? special-form?) (analyze-sf (name op) args))
-         ((? intrinsic?) (analyze-intr (name op) args))
-         (_ (make-App (analyze op) (map analyze args)))))
+      (((and (? special-form?) op) . args) (analyze-sf (name op) args))
+      (((and (? intrinsic?) op) . args)    (analyze-intr (name op) args))
+      ((op . args)                         (make-App (analyze op)
+                                                     (map analyze args)))
       
-      ((? symbol?) (make-Var sexp))
+      ((? symbol?)                   (make-Var sexp))
       ((or (? fixnum?) (? boolean?)) (make-Const sexp))
+      
       (_ (error "invalid expression" sexp))))
 
   (define (analyze-sf name args)
@@ -47,14 +62,24 @@
       (('if cond then else) (make-If (analyze cond)
                                      (analyze then)
                                      (analyze else)))
+      (('do . stmts) (make-Do (map analyze stmts)))
+      
       (('fn formals types body) (make-Fn formals types
                                          (analyze body)))
+      (('letfn defns body)
+       (make-Fix (map (cute update-defnbody analyze <>) defns) (analyze body)))
+      (('def name val) (make-Def name (analyze val)))
+
+      (('quote val) (make-Const val))
+      (('halt val) (make-Halt (analyze val)))
+      
       (form (error "invalid special form" form))))
 
   (define (analyze-intr name args)
     (match (cons name args)
       (((or 'add 'sub 'mul 'div) a b)
        (make-Primop name `(,(analyze a) ,(analyze b))))
+      
       (form (error "invalid intrinsic" form))))
 
   ;;;; Convert Back to Sexpr (for Debugging)
@@ -64,11 +89,21 @@
       (($ If cond then else) `($if ,(core->sexp cond)
                                    ,(core->sexp then)
                                    ,(core->sexp else)))
+      (($ Do stmts) `($do ,@(map core->sexp stmts)))
+      
       (($ Fn formals types body) `($fn ,formals ,types
                                        ,(core->sexp body)))
+      (($ Fix defns body)
+       `($letfn ,(map (cute update-defnbody core->sexp <>) defns)
+                ,(core->sexp body)))
+      (($ Def name val) `($def ,name ,(core->sexp val)))
+      
       (($ Primop op args)
        `(,(string->symbol (string-append "%" (symbol->string op)))
          ,@(map core->sexp args)))
       (($ App callee args) `(,(core->sexp callee) ,@(map core->sexp args)))
+
+      (($ Halt val) `($halt ,(core->sexp val)))
+      
       (($ Var name) name)
       (($ Const val) val))))
